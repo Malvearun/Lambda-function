53e8b1c33a9e412da00e0e461a971f1d8bc0ddd0
Malvearun <37962813+Malvearun@users.noreply.github.com>
2020-09-08 13:07:04 +0100

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -2,4 +2,7 @@ import os
 
 
 def lambda_handler(event, context):
-    return "{} from Lambda!".format(os.environ['greeting'])
+    return "{} from Lambda function!".format(os.environ['Good Day!'])
+
+
+# reference: https://docs.aws.amazon.com/lambda/latest/dg/with-sns-create-package.html
